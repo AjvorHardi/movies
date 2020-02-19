2e03edf1b549e65a38da3507b01fd397e17589ff
AjvorHardi <markovic.goran@outlook.com>
2020-02-19 10:49:07 +0100

    Zadatak 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    @foreach ($movies as $movie)
-        <a href="movies/{{$movie->id}}"><h3>{{ $movie->title }}</h3></a>
-        <p>{{ $movie->storyline }}</p>
-    @endforeach
-</body>
-</html>
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+    @if ($movies)
+        @foreach ($movies as $movie)
+            <a href="movies/{{ $movie->id }}"><h3>{{ $movie->title }}</h3></a>
+            <p>{{ $movie->storyline }}</p>
+        @endforeach
+    @endif
+    </div>
+@endsection
